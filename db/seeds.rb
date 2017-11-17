d568fd37e654560b2ce18d3322577cfa2670f3fa
Justin Valentin <Justinvalentine88@gmail.com>
2017-11-17 12:42:13 -0500

    fixed seeding error

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@ Product.delete_all
 Product.create! id: 1, name: "Banana", price: 0.49, active: true
 Product.create! id: 2, name: "Apple", price: 0.29, active: true
 Product.create! id: 3, name: "Carton of Strawberries", price: 2.99, active: true
-Product.create! id: 2, name: "Orange", price: 0.39, active: true
-Product.create! id: 3, name: "Kiwi", price: 0.99, active: true
+Product.create! id: 4, name: "Orange", price: 0.39, active: true
+Product.create! id: 5, name: "Kiwi", price: 0.99, active: true
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
