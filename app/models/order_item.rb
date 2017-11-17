552fd4a206d00cc98fb3fe0d52872d4c29248f3d
Justin Valentin <Justinvalentine88@gmail.com>
2017-11-17 11:58:55 -0500

    fixed errors

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,7 +1,7 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
-  
+
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validate :product_present
   validate :order_present
@@ -37,5 +37,4 @@ private
     self[:unit_price] = unit_price
     self[:total_price] = quantity * self[:unit_price]
   end
-end
 end
