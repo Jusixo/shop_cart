2045213ab1208bb0c0ffbc0d5c2cdb80fffa7234
Justin Valentin <Justinvalentine88@gmail.com>
2017-11-17 13:23:01 -0500

    updated cart show

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,6 +6,18 @@ class OrderItemsController < ApplicationController
     session[:order_id] = @order.id
   end
 
+  def create
+    @order = current_order
+    @order_item = @order.order_items.new(order_item_params)
+    existing_order = @order.order_items.where(product_id: params[:order_item][:product_id])
+      if existing_order.count >= 1
+        existing_order.last.update_column(:quantity, existing_order.last.quantity + params[:order_item][:quantity].to_i)
+      else
+        @order.save
+      end
+    session[:order_id] = @order.id
+  end
+
   def update
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
@@ -21,7 +33,7 @@ class OrderItemsController < ApplicationController
   end
 
   private
-  
+
   def order_item_params
     params.require(:order_item).permit(:quantity, :product_id)
   end
